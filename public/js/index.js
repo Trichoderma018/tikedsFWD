76c04ed5e054d5ca00a42c00952d2152f9435afc
juanpa <juan.paq72@mail.com>
2025-02-25 10:52:08 -0600

    Configuraciones

---
+++ public/js/index.js
@@ -0,0 +1,12 @@
+// sumar
+function sumar (num1, num2) {
+    return num1+num2
+}
+function restar (num1, num2) {
+    return num1-num2
+}
+function multiplicar (num1, num2) {
+    return num1*num2
+}
+export {sumar, restar, multiplicar}
+
